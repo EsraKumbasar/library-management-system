ede518958a58f99c22dbb9d1d2a00cd38faa4e43
EsraKumbasar <esrakumbasar002@gmail.com>
2021-05-17 00:42:07 +0300

    initial commit

---
+++ Library/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Library.Models.Entity;
+
+namespace Library.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(TBLADMIN p)
+        {
+            var bilgiler = db.TBLADMIN.FirstOrDefault(x => x.KULLANICI == p.KULLANICI &&
+            x.SIFRE == p.SIFRE);
+
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.KULLANICI, false);
+                Session["Kullanici"] = bilgiler.KULLANICI.ToString();
+                return RedirectToAction("Index", "Kategori");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Library/Models/Siniflarim/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Library.Models.Entity;
+namespace Library.Models.Siniflarim
+{
+    public class Class1
+    {
+        public IEnumerable<TBLKITAP> Deger1 { get; set; }
+        public IEnumerable<HAKKIMIZDA> Deger2 { get; set; }
+    }
+}
